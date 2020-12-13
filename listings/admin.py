282f8daf7016e06ee492af7d12ddb644a8546e16
tirth <tirthcoder@gmail.com>
2020-12-13 16:46:15 +0530

    Admin template and customize style for admin added

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . models import Listing
+
+# For customizig listing when admin clicks listing how page should look like
+
+
+class ListingAdmin(admin.ModelAdmin):
+    # What items to display
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    # What items needs to be clickable to go through
+    list_display_links = ('id', 'title')
+    # Filter by realtor
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode', 'price')
+    list_per_page = 25   # There should be 25 list only if max then pagination
+
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Realtor
+
+# For customizig listing when admin clicks listing how page should look like
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
 
 # Register your models here.
+admin.site.register(Realtor, RealtorAdmin)
