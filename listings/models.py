f813bb1025d2445ba265a74139508697dc7a43b5
tirth <tirthcoder@gmail.com>
2020-12-13 15:11:54 +0530

    Added models for listings and realtors

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
+
+
+class Listing(models.Model):
+    # if realtor is deleted by admin then listing of that realtor should not be delete
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    # blank = True means this field is optional
+    # TextField is larger than CharField
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(
+        max_digits=2, decimal_places=1)  # Only max 2 bath
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    # Main field to display for admin
+
+    def __str__(self):
+        return self.title
