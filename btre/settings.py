c0308dadb1461e67939cd8afa60b71db0775a041
tirth <tirthcoder@gmail.com>
2020-12-13 14:15:03 +0530

    Connected with Postgres

--- btre/settings.py
+++ btre/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'hanuman18',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
