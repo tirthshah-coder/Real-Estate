9bf49e9c417b1781605c79ce313c814df1d921d4
Tirth <tirthcoder@gmail.com>
2020-12-18 15:57:17 +0530

    Inquiry form with email done

--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'techni621@gmail.com'
+EMAIL_HOST_PASSWORD = 'hanuman18'
+EMAIL_USE_TLS = True
