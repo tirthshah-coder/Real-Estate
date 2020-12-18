745f258c44115196213c9796cf1ab57d88814d9e
Tirth <tirthcoder@gmail.com>
2020-12-18 13:35:20 +0530

    Contacts app

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'pages.apps.PagesConfig',
